891370f4476690bda88a58d79744b7dbf5d52c3a
Sumit Anand <anandsumit2000@gmail.com>
2025-08-22 17:10:48 +0530

    Add configurable host/port settings for cross-environment deployment (local/Docker/k8s)

--- app/config.py
+++ app/config.py
@@ -1,3 +1,4 @@
+
 """Module for configuration settings.
 - loads environment variables using dotenv
 - sets up various configuration parameters for the Flask app.
@@ -7,9 +8,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-if os.environ.get('FLASK_ENV') == 'testing':
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
-else:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+class Config:
+    if os.environ.get('FLASK_ENV') == 'testing':
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    else:
+        SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:///orders.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
